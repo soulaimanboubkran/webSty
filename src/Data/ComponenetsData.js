24c48898b4a39cfbeddd76b33cc775f56521ee72
soulaiman bouiboukirn <soulaiman.soulb@gmail.com>
2024-08-15 16:31:15 +0100

    commit imgs

--- src/Data/ComponenetsData.js
+++ src/Data/ComponenetsData.js
@@ -3,84 +3,84 @@ export const Componentsdata = [
         id:1,
         link:'headers',
         title:'Navbar',
-        thubmnail:'https://daisyui.com/images/components/navbar.webp'
+        thubmnail:'https://img.daisyui.com/images/components/navbar.webp'
 
     },
     {
         id:2,
         link:'footer',
         title:'Footer',
-        thubmnail:'https://daisyui.com/images/components/footer.webp'
+        thubmnail:'https://img.daisyui.com/images/components/footer.webp'
 
     },
     {
         id:3,
         link:'/card',
         title:'Card',
-        thubmnail:'https://daisyui.com/images/components/card.webp'
+        thubmnail:'https://img.daisyui.com/images/components/card.webp'
 
     },
     {
         id:4,
         link:'/hero',
         title:'Hero',
-        thubmnail:'https://daisyui.com/images/components/hero.webp'
+        thubmnail:'https://img.daisyui.com/images/components/hero.webp'
 
     },
     {
         id:5,
         link:'/input_file',
         title:'File Input',
-        thubmnail:'https://daisyui.com/images/components/file-input.webp'
+        thubmnail:'https://img.daisyui.com/images/components/file-input.webp'
 
     },
     {
         id:6,
         link:'/loading',
         title:'Loading',
-        thubmnail:'https://daisyui.com/images/components/loading.webp'
+        thubmnail:'https://img.daisyui.com/images/components/loading.webp'
 
     },
     {
         id:7,
         link:'/menu',
         title:'Menu',
-        thubmnail:'https://daisyui.com/images/components/menu.webp'
+        thubmnail:'https://img.daisyui.com/images/components/menu.webp'
 
     },
     {
         id:6,
         link:'/loading',
         title:'Loading',
-        thubmnail:'https://daisyui.com/images/components/loading.webp'
+        thubmnail:'https://img.daisyui.com/images/components/loading.webp'
 
     },
     {
         id:7,
         link:'/chat_bubble',
         title:'Chat bubble',
-        thubmnail:'https://daisyui.com/images/components/chat.webp'
+        thubmnail:'https://img.daisyui.com/images/components/chat.webp'
 
     },
     {
         id:8,
         link:'/checkbox',
         title:'Checkbox',
-        thubmnail:'https://daisyui.com/images/components/checkbox.webp'
+        thubmnail:'https://img.daisyui.com/images/components/checkbox.webp'
 
     },
     {
         id:9,
         link:'/countdown',
         title:'Countdown',
-        thubmnail:'https://daisyui.com/images/components/countdown.webp'
+        thubmnail:'https://img.daisyui.com/images/components/countdown.webp'
 
     },
     {
         id:9,
         link:'/Avatar',
         title:'Avatar',
-        thubmnail:'https://daisyui.com/images/components/countdown.webp'
+        thubmnail:'https://img.daisyui.com/images/components/countdown.webp'
 
     }
 ]
