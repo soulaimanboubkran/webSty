e4b1277ae53e9893b553350e3f83227e5ffb1574
soulaiman bouiboukirn <soulaiman.soulb@gmail.com>
2024-08-15 20:29:39 +0100

    commit

--- src/Components/Components.jsx
+++ src/Components/Components.jsx
@@ -16,7 +16,7 @@ const Components = () => {
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {Componentsdata.map((item, index) => (
               <Link
-                to={`websty.vercel.app/${item.link}`}
+                to={`/${item.link}`}
                 key={index}
                 className="card p-1 bg-base-100 border hover:shadow-xl"
               >
